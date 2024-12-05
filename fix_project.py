9dc23bcaebfe891c5411626c953c1089f314a5ee
elimuliani <137256683+elimuliani@users.noreply.github.com>
2024-12-05 10:02:45 +0700

    Update fix_project.py

--- fix_project.py
+++ fix_project.py
@@ -76,11 +76,11 @@ for i, (category, color) in enumerate(categories.items()):
             # Add clickable pagination buttons
             col1, col2, col3 = st.columns([1, 2, 1])
             with col1:
-                if st.button("➖", key=f"prev_{category}"):
+                if st.button("➖", key=f"prev_{category}", help="Halaman Sebelumnya", use_container_width=False):
                     if current_page > 1:
                         st.session_state[f"page_{category}"] -= 1
             with col3:
-                if st.button("➕", key=f"next_{category}"):
+                if st.button("➕", key=f"next_{category}", help="Halaman Berikutnya", use_container_width=False):
                     if current_page < total_pages:
                         st.session_state[f"page_{category}"] += 1
             with col2:
